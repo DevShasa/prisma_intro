2118521288513874cc0cb4ab1e20e9984591137a
DevShasa <shasa.thuo@gmail.com>
2023-01-29 16:26:32 +0300

    completed basic prisma intro with crud

--- index.ts
+++ index.ts
@@ -17,10 +17,32 @@ app.post("/", async (req:Request, res:Response)=>{
     res.json(user); // return the created user
 }); 
 
+app.post("/createcar",async(req:Request, res:Response)=>{
+    const { model, year, userId } = req.body
+    const car = await prisma.car.create({
+        data:{
+            model:model,
+            year:year,
+            userId: userId
+        }
+    })
+    res.json(car)
+})
+
 app.get("/", async(req:Request, res:Response)=>{
-    const users = await prisma.user.findMany(); // return everything inside the user table
+    const users = await prisma.user.findMany({include:{cars:true}});
     res.json(users);
 }); 
+
+app.get("/fetchUserbyId/:id", async(req:Request, res:Response)=>{
+    const id = req.params.id
+    const user = await prisma.user.findUnique({
+        where:{id:Number(id)}
+    })
+    res.json(user)
+})
+
+
 
 app.put("/", async(req:Request, res:Response)=>{
     const { id, username } = req.body
@@ -37,7 +59,17 @@ app.delete("/:id", async(req:Request, res:Response)=>{
         where:{id:id},
     })
     res.json({message:`user ${id} has been deleted`})
-}); 
+});
+
+
+// does not work with sqlite
+// app.post("/createManyUsers", async(req:Request, res:Response)=>{
+//     const { userList } = req.body
+//     const users = await prisma.user.createMany({
+//         data:userList
+//     })
+//     res.json(users);
+// })
 
 app.listen(3001,()=>{
     console.log("THE SERVER IS LISTENING @3001 WADDUP")
